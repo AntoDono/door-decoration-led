bae0b69ad7403b87eef08da76d37e047e25bd354
AntoDono <anthony20151128@gmail.com>
2022-12-13 22:24:20 -0500

    temp fix

--- bindings/python/samples/gif-viewer.py
+++ bindings/python/samples/gif-viewer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import time
 import sys
+from samplebase import SampleBase
+
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
@@ -11,50 +13,65 @@ from PIL import Image
 # else:
 #     image_file = sys.argv[1]
 
-gif = Image.open("../../../merryxmas.gif")
 
-try:
-    num_frames = gif.n_frames
-except Exception:
-    sys.exit("provided image is not a gif")
+class GifViewer(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(GifViewer, self).__init__(*args, **kwargs)
+        self.parser.add_argument("-p", "--path", help="Path of the gif", default="../../../merryxmas.gif")
+
+    def run(self):
+        gif = Image.open(self.args.path)
+
+        try:
+            num_frames = gif.n_frames
+        except Exception:
+            sys.exit("provided image is not a gif")
 
 
-# Configuration for the matrix
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 32
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+        # Configuration for the matrix
+        options = RGBMatrixOptions()
+        options.rows = 32
+        options.cols = 32
+        options.chain_length = 1
+        options.parallel = 1
+        options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
 
-matrix = RGBMatrix(options = options)
+        matrix = RGBMatrix(options = options)
 
-# Preprocess the gifs frames into canvases to improve playback performance
-canvases = []
-print("Preprocessing gif, this may take a moment depending on the size of the gif...")
-for frame_index in range(0, num_frames):
-    gif.seek(frame_index)
-    # must copy the frame out of the gif, since thumbnail() modifies the image in-place
-    frame = gif.copy()
-    frame.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-    canvas = matrix.CreateFrameCanvas()
-    canvas.SetImage(frame.convert("RGB"))
-    canvases.append(canvas)
-# Close the gif file to save memory now that we have copied out all of the frames
-gif.close()
+        # Preprocess the gifs frames into canvases to improve playback performance
+        canvases = []
+        print("Preprocessing gif, this may take a moment depending on the size of the gif...")
+        for frame_index in range(0, num_frames):
+            gif.seek(frame_index)
+            # must copy the frame out of the gif, since thumbnail() modifies the image in-place
+            frame = gif.copy()
+            frame.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+            canvas = matrix.CreateFrameCanvas()
+            canvas.SetImage(frame.convert("RGB"))
+            canvases.append(canvas)
+        # Close the gif file to save memory now that we have copied out all of the frames
+        gif.close()
 
-print("Completed Preprocessing, displaying gif")
+        print("Completed Preprocessing, displaying gif")
 
-try:
-    print("Press CTRL-C to stop.")
+        try:
+            print("Press CTRL-C to stop.")
 
-    # Infinitely loop through the gif
-    cur_frame = 0
-    while(True):
-        matrix.SwapOnVSync(canvases[cur_frame], framerate_fraction=10)
-        if cur_frame == num_frames - 1:
+            # Infinitely loop through the gif
             cur_frame = 0
-        else:
-            cur_frame += 1
-except KeyboardInterrupt:
-    sys.exit(0)
+            while(True):
+                matrix.SwapOnVSync(canvases[cur_frame], framerate_fraction=10)
+                if cur_frame == num_frames - 1:
+                    cur_frame = 0
+                else:
+                    cur_frame += 1
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+# Main function
+if __name__ == "__main__":
+    gif_viewer = GifViewer()
+    if (not gif_viewer.process()):
+        gif_viewer.print_help()
+
+
