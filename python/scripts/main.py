fddddbfa2c0bcb9bbedc006bc93201b2c45fdd5a
AntoDono <anthony20151128@gmail.com>
2022-12-14 00:27:50 -0500

    music

--- python/scripts/main.py
+++ python/scripts/main.py
@@ -4,5 +4,5 @@ from music import Music
 import os.path as path
 
 music = Music()
-print(path.abspath(path.join(__file__, "../../../assets/mp3/Merry_Christmas.mp4")))
-music.play(path.abspath(path.join(__file__, "../../../assets/mp3/Merry_Christmas.mp4")))
+print(path.abspath(path.join(__file__, "../../../assets/mp3/Merry_Christmas.mp3")))
+music.play(path.abspath(path.join(__file__, "../../../assets/mp3/Merry_Christmas.mp3")))
