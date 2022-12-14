64aaa5935067c79a972877dd7fbba3ff1b9b9558
AntoDono <anthony20151128@gmail.com>
2022-12-14 00:23:06 -0500

    music

--- python/scripts/main.py
+++ python/scripts/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from gifViewer import GifViewer
 from music import Music
+import os.path as path
 
 music = Music()
-music.play("../../assets/Merry_Christmas.mp4")
+music.play(path.join(__file__, "../../assets/mp3/Merry_Christmas.mp4"))
