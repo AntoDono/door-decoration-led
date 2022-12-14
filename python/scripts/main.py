7fcf7a320b072d57fb8db94af227a23d3072ad43
AntoDono <anthony20151128@gmail.com>
2022-12-14 00:56:36 -0500

    music

--- python/scripts/main.py
+++ python/scripts/main.py
@@ -5,7 +5,7 @@ import os.path as path
 import pygame
 
 music = Music()
-music.play("Merry_Christmas.wav")
+music.play("Merry_Christmas.mp3")
 # print(path.abspath(path.join(__file__, "../../../assets/mp3/Merry_Christmas.mp3")))
 
 
