a17573e93cae917c86b903c615d8aba6f78126ff
AntoDono <anthony20151128@gmail.com>
2022-12-14 00:47:45 -0500

    music

--- python/scripts/main.py
+++ python/scripts/main.py
@@ -2,7 +2,13 @@
 # from gifViewer import GifViewer
 from music import Music
 import os.path as path
+import pygame
 
-music = Music()
+# music = Music()
+# music.play("Merry_Christmas.wav")
 # print(path.abspath(path.join(__file__, "../../../assets/mp3/Merry_Christmas.mp3")))
-music.play("Merry_Christmas.wav")
+
+pygame.mixer.init()
+crash_sound = pygame.mixer.Sound("Merry_Christmas.wav")
+crash_sound.play()
+
--- python/scripts/music.py
+++ python/scripts/music.py
@@ -3,11 +3,13 @@ import pygame
 class Music():
     def __init__(self):
         pygame.mixer.init()
+        pygame.mixer.music.set_volume(0.7)
         self.song = None
 
     def play(self, path):
         self.song = pygame.mixer.Sound(path)
         self.song.play()
+        print("Done playing")
 
     def pause(self):
         self.song.pause()
