5e6b6d84de069ab16c924d03a03c7aa1cd16391d
Anthony Zhen <anthony20151128@gmail.com>
2022-12-15 12:26:43 -0500

    testing killing

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def process_command(thread_type, arg):
         led_process = subprocess.Popen("python3 test.py", shell=True)
 
 def kill_process(pid):
-    os.kill(pid, signal.SIGTERM)
+    os.kill(pid, signal.CTRL_C_EVENT)
 
 def led_timer(millseconds):
     global led_process
