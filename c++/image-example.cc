03bc5e896caf76b7dd0f2340c16ef461d6a45c5a
AntoDono <anthony20151128@gmail.com>
2022-12-14 19:02:44 -0500

    testing borders

--- c++/image-example.cc
+++ c++/image-example.cc
@@ -80,7 +80,8 @@ static ImageVector LoadImageAndScaleImage(const char *filename,
 // interface as well as the FrameCanvas we use in the double-buffering of the
 // animted image.
 void CopyImageToCanvas(const Magick::Image &image, Canvas *canvas) {
-  const int offset_x = abs((canvas->width() - image.size().width)/2), offset_y = abs((canvas->height() - image.size().height)/2);  // If you want to move the image.
+  // const int offset_x = abs((canvas->width() - image.size().width)/2), offset_y = abs((canvas->height() - image.size().height)/2);  // If you want to move the image.
+  const int offset_x = canvas->width(), offset_y = canvas->height();  // If you want to move the image.
   // Copy all the pixels to the canvas.
   for (size_t y = 0; y < image.rows(); ++y) {
     for (size_t x = 0; x < image.columns(); ++x) {
