56199bd1d4611d17f7981fe66f436737c61836bb
Anthony Zhen <anthony20151128@gmail.com>
2022-12-15 22:30:23 -0500

    psutil

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ LED_SHOW = [
     # {"arg": ["./scripts/image-infinite", "./assets/images/sled1.gif"], "duration": 10000, "music": None},
     # {"arg": ["./scripts/image-infinite", "./assets/images/tree1.gif"], "duration": 10000, "music": None},
     {"arg": ["./scripts/scroll-text", "Happy Holidays! -f ./fonts/clR6x12.bdf -s 5 -l -1 -y 7"], "duration": 10000, "music": None},
-    # {"arg": ["./scripts/image-infinite", "./assets/images/sign1.gif"], "duration": 10000, "music": None},
+    {"arg": ["./scripts/image-infinite", "./assets/images/sign1.gif"], "duration": 10000, "music": None},
     {"arg": ["./scripts/image-infinite", "./assets/images/xmas1.jpg"], "duration": 10000, "music": None},
     {"arg": ["./scripts/image-infinite", "./assets/images/xmas2.jpg"], "duration": 10000, "music": None},
     {"arg": ["./scripts/image-infinite", "./assets/images/tree1.gif"], "duration": 10000, "music": None},
@@ -63,14 +63,14 @@ def process_led_command(arg):
     if (not isinstance(arg, list)):
         Exception("Process LED takes a list as argument")
 
-    command = f"sudo {arg[0]} {arg[1]} --led-rows {LED_ROWS} --led-cols {LED_COLS} --led-brightness {LED_BRIGHTNESS} --led-slowdown-gpio {LED_SLOWDOWN_GPIO} --led-limit-refresh {LED_REFRESH_LIMIT}"
+    command = f"{arg[0]} {arg[1]} --led-rows {LED_ROWS} --led-cols {LED_COLS} --led-brightness {LED_BRIGHTNESS} --led-slowdown-gpio {LED_SLOWDOWN_GPIO} --led-limit-refresh {LED_REFRESH_LIMIT}"
     if (LED_NO_HARDWARE_PULSE):
         command += " --led-no-hardware-pulse"
     print(command)
     return command
 
 def process_music_command(filepath):
-    command = f"mpg123 {filepath} -f {VOLUME}"
+    command = f"mpg123 {filepath} -f {VOLUME} --aggressive"
     return command
 
 def process_command(thread_type, arg):
