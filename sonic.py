8788b53873ccf700f23d7a3f114078d82aa0ede7
Anthony Zhen <anthony20151128@gmail.com>
2022-12-21 10:39:56 -0500

    wtf

--- sonic.py
+++ sonic.py
@@ -13,31 +13,29 @@ GPIO.setup(ULTRA_PIN_IN, GPIO.IN)
 
 while True:
         # set Trigger to HIGH
-        # GPIO.output(ULTRA_PIN_OUT, True)
+        GPIO.output(ULTRA_PIN_OUT, True)
     
         # # set Trigger after 0.01ms to LOW
-        # time.sleep(0.00001)
-        # GPIO.output(ULTRA_PIN_OUT, False)
+        time.sleep(0.00001)
+        GPIO.output(ULTRA_PIN_OUT, False)
 
-        # StartTime = time.time()
-        # StopTime = time.time()
+        StartTime = time.time()
+        StopTime = time.time()
 
-        print(GPIO.input(ULTRA_PIN_IN))
-    
         # # save StartTime
-        # while GPIO.input(ULTRA_PIN_IN) == 0:
-        #     print(f"Input: {GPIO.input(ULTRA_PIN_IN)}")
-        #     StartTime = time.time()
+        while GPIO.input(ULTRA_PIN_IN) == 0:
+            print(f"Input: {GPIO.input(ULTRA_PIN_IN)}")
+            StartTime = time.time()
     
         # # save time of arrival
-        # while GPIO.input(ULTRA_PIN_IN) == 1:
-        #     print("Waiting for 0")
-        #     StopTime = time.time()
+        while GPIO.input(ULTRA_PIN_IN) == 1:
+            print("Waiting for 0")
+            StopTime = time.time()
     
         # # time difference between start and arrival
-        # TimeElapsed = StopTime - StartTime
-        # # multiply with the sonic speed (34300 cm/s)
-        # # and divide by 2, because there and back
-        # distance = (TimeElapsed * 34300) / 2
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
 
-        # print(f"Distance: {distance}")
+        print(f"Distance: {distance}")
